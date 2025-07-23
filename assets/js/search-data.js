c53957f93e851b0155d24fd3e859c08884b28c1c
pinakirm <33491031+pinakirm@users.noreply.github.com>
2025-07-23 22:49:02 +0000

    Deploying to gh-pages from @ pinakirm/pinakirm.github.io@4cf77f5d33e2118a64b623150748f325f9c5ffcb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -45,8 +45,8 @@ ninja.data = [{
             window.location.href = "/cv/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          title: "Teaching",
+          description: "A summary of my teaching assistantships at Purdue University.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
