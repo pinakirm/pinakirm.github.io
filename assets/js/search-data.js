6f433c70759b279291cbe904b91fd499838eae91
pinakirm <33491031+pinakirm@users.noreply.github.com>
2025-07-24 00:17:05 +0000

    Deploying to gh-pages from @ pinakirm/pinakirm.github.io@0e5540b87b17c32c7215c909aed731a10a41d360 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -463,9 +463,9 @@ ninja.data = [{
           description: "Personal Project | Website to help Graduate Teaching Assistants (GTAs) manage, change, and analyze student grades using interactive graphs.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
