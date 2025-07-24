5481e1cd2d94a55a546aa147e1f43a7f3ed5a705
pinakirm <33491031+pinakirm@users.noreply.github.com>
2025-07-24 00:05:39 +0000

    Deploying to gh-pages from @ pinakirm/pinakirm.github.io@f459ae7a000f718da2a8cf438a73c7b904c55ede 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -453,9 +453,9 @@ ninja.data = [{
           description: "A Java program to parse and evaluate infix expressions using stacks",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-quicksort-analyzer",
+          title: 'Quicksort Analyzer',
+          description: "Java implementation to analyze the effect of pivot choice in Quicksort",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
